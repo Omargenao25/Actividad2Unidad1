2667f776be5243207f4a23a034751637ca9a4993
Omar Genao <omargenao78@gmail.com>
2025-05-29 23:31:59 -0400

    comit Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// LoginController.cs
+public class LoginController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Ingresar(string usuario, string contrasena)
+    {
+        if (EsUsuarioValido(usuario, contrasena))
+        {
+            return RedirectToAction("Bienvenido");
+        }
+        else
+        {
+            ViewBag.Mensaje = "Usuario incorrecto";
+            return View();
+        }
+    }
+
+    public ActionResult Bienvenido()
+    {
+        return View();
+    }
+
+    private bool EsUsuarioValido(string usuario, string contrasena)
+    {
+        return usuario == "admin" && contrasena == "123456";
+    }
+}
+
+    
+
