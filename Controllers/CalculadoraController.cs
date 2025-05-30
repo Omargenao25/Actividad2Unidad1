ef702cc93963fd0f5c261c365e7fddf91e67b623
Omar Genao <omargenao78@gmail.com>
2025-05-29 22:31:54 -0400

    commit calculadora

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: CalculadoraController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculadora()
+        {
+            return View();
+        }
+
+        public ActionResult Calcular(double valor1, double valor2, string operacion)
+        {
+            double resultado = 0;
+            switch (operacion)
+            {
+                case "+":
+                    resultado = valor1 + valor2;
+                    break;
+                case "-":
+                    resultado = valor1 - valor2;
+                    break;
+                case "*":
+                    resultado = valor1 * valor2;
+                    break;
+                case "/":
+                    resultado = valor1 / valor2;
+                    break;
+            }
+            ViewBag.Resultado = resultado;
+            return View("Calculadora");
+        }
+    }
+}
