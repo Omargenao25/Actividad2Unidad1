16e1ad4262f0e883206611de9022536c8cc38906
Omar Genao <omargenao78@gmail.com>
2025-05-29 22:00:29 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Saludo(string nombre)
+        {
+            ViewBag.Mensaje = $"Hola, {nombre}, bienvenido a ASP.NET MVC.";
+            return View();
+        }
+
+    }
+}
