8971053a47732c658c9a30badd2a25a2b72f9e91
Omar Genao <omargenao78@gmail.com>
2025-05-29 23:12:56 -0400

    commit Evaluacion

---
+++ Controllers/EvaluacionController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class EvaluacionController : Controller
+    {
+        // GET: EvaluacionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalcularPromedio(decimal calificacion1, decimal calificacion2, decimal calificacion3, decimal calificacion4)
+        {
+            decimal promedio = CalcularPromedioCalificaciones(calificacion1, calificacion2, calificacion3, calificacion4);
+            string estado = ObtenerEstadoEstudiante(promedio);
+            ViewBag.Promedio = promedio;
+            ViewBag.Estado = estado;
+            return View();
+        }
+
+        private decimal CalcularPromedioCalificaciones(decimal c1, decimal c2, decimal c3, decimal c4)
+        {
+            return (c1 + c2 + c3 + c4) / 4;
+        }
+
+        private string ObtenerEstadoEstudiante(decimal promedio)
+        {
+            if (promedio >= 70)
+                return "Aprobado";
+            else if (promedio >= 60)
+                return "En recuperación";
+            else
+                return "Reprobado";
+        }
+    }
+}
+
