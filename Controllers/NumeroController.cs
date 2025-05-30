8a9c96b1b406ec5ec695c63dc58d0735187e4524
Omar Genao <omargenao78@gmail.com>
2025-05-29 22:43:02 -0400

    commit numero primo

---
+++ Controllers/NumeroController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class NumeroController : Controller
+    {
+        // GET: NumeroController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EsPrimo(int numero)
+        {
+            bool esPrimo = EsNumeroPrimo(numero);
+            ViewBag.Mensaje = esPrimo
+                ? $"El número {numero} es primo."
+                : $"El número {numero} no es primo.";
+            return View();
+        }
+
+        private bool EsNumeroPrimo(int numero)
+        {
+            if (numero <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
